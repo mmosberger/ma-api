8f7e2efcd317c72f5267e6adf9a9f30599ef84b0
Knirpsii <michel.j.mosberger@gmail.com>
2021-08-15 17:47:25 +0200

    -

--- api/routers/test.routes.js
+++ api/routers/test.routes.js
@@ -14,12 +14,12 @@ const test = require("../controllers/test.controller.js")
 router.get('/test/:id', test.getTest)
 
 router.patch('/test/:id/sleep',
-    body('finished').isInt({ min: 0, max: 2 }),
-    body('drugs').isInt({ min: 0, max: 1}),
-    body('sleep_quality').isInt({ min: 1, max: 10}),
-    body('stress').isInt({ min: 0, max: 1}),
-    body('start_sleep').isDate(), //TODO in welchem Format werden die Daten des Schlafes an die API geschickt?
-    body('end_sleep').isDate(),
+    body('finished').isInt({ min: 0, max: 2 }).withMessage(""),
+    body('drugs').isInt({ min: 0, max: 1}).withMessage("Bitte gib an, ob du gestern Abend Alkohol oder Nikotin zu dir genommen hast."),
+    body('sleep_quality').isInt({ min: 1, max: 10}).withMessage("Bitte gib auf der Skala von 1-10 an, wie gut du geschlafen hast."),
+    body('stress').isInt({ min: 0, max: 1}).withMessage("Bitte gib an, ob du gestern Abend jegliche Art von Stress hattest."),
+    body('start_sleep').isDate().withMessage("Bitte gib das Datum und die Uhrzeit an, wann du Schlafen gegangen bist."), //TODO in welchem Format werden die Daten des Schlafes an die API geschickt?
+    body('end_sleep').isDate().withMessage("Bitte gib das Datum und die Uhrzeit an, wann du heute Morgen aufgewacht bist"), //TODO Ist das Datum hier nötig?
     test.patchQuestions
 )
 
