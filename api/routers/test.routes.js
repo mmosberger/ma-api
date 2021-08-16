f4ea353bc69e3f6429c94ffe94323d60e7c2b51e
Knirpsii <michel.j.mosberger@gmail.com>
2021-08-16 22:17:49 +0200

    implemented validation in middleware

--- api/routers/test.routes.js
+++ api/routers/test.routes.js
@@ -5,33 +5,20 @@
  *@since 12.08.2021
  */
 
-const router = require('express').Router()
-const {body} = require('express-validator');
-
+const router  = require('express').Router()
 
 const test = require("../controllers/test.controller.js")
+const {checkSleep, checkTest} = require("../middleware/validation");
 
 router.get('/test/:id', test.getTest)
 
-router.patch('/test/:id/sleep',
-    body('finished').isInt({ min: 0, max: 2 }).withMessage(""),
-    body('drugs').isInt({ min: 0, max: 1}).withMessage("Bitte gib an, ob du gestern Abend Alkohol oder Nikotin zu dir genommen hast."),
-    body('sleep_quality').isInt({ min: 1, max: 10}).withMessage("Bitte gib auf der Skala von 1-10 an, wie gut du geschlafen hast."),
-    body('stress').isInt({ min: 0, max: 1}).withMessage("Bitte gib an, ob du gestern Abend jegliche Art von Stress hattest."),
-    body('start_sleep').isDate().withMessage("Bitte gib das Datum und die Uhrzeit an, wann du Schlafen gegangen bist."), //TODO in welchem Format werden die Daten des Schlafes an die API geschickt?
-    body('end_sleep').isDate().withMessage("Bitte gib das Datum und die Uhrzeit an, wann du heute Morgen aufgewacht bist"), //TODO Ist das Datum hier nötig?
-    test.patchQuestions
-)
+router.patch('/test/:id/sleep', checkSleep, test.sleepQuestions)
 
 // finished, sleep_start, sleep_end, sleep_quality, stress, drugs
 
 
-router.patch('/test/:id',
-    body('userInput').isArray(),
-    body('time_taken').isInt(), //TODO sollte in sec gesendet werden
-    test.updateTest)
+router.patch('/test/:id', checkTest, test.updateTest)
 
-//TODO FRage: Werden alle Testantworten auf 1 mal gesendet?
 //TODO gute Websiten: https://blog.logrocket.com/node-js-express-js-mysql-rest-api-example/ https://github.com/geshan/programming-langugages-api
 
 module.exports = router
