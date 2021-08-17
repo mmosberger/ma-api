5706d7ba6f402244ca2ac65c28702285f3fb3c9a
Knirpsii <michel.j.mosberger@gmail.com>
2021-08-17 14:43:53 +0200

    refactored routes

--- api/routers/test.routes.js
+++ api/routers/test.routes.js
@@ -7,6 +7,7 @@
 
 const router  = require('express').Router()
 
+
 const test = require("../controllers/test.controller.js")
 const {checkSleep, checkTest} = require("../middleware/validation");
 
@@ -14,11 +15,7 @@ router.get('/test/:id', test.getTest)
 
 router.patch('/test/:id/sleep', checkSleep, test.sleepQuestions)
 
-// finished, sleep_start, sleep_end, sleep_quality, stress, drugs
-
-
 router.patch('/test/:id', checkTest, test.updateTest)
 
-//TODO gute Websiten: https://blog.logrocket.com/node-js-express-js-mysql-rest-api-example/ https://github.com/geshan/programming-langugages-api
 
 module.exports = router
