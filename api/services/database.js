d32fde42cd99b9a1da32c837f0790baa2eb71463
Knirpsii <michel.j.mosberger@gmail.com>
2021-09-02 22:48:30 +0200

    -

--- api/services/database.js
+++ api/services/database.js
@@ -85,6 +85,11 @@ class Database {
         return await this.query(queryString, queryValues);
     }
 
+    static checkTests = async () => {
+        let queryString = 'SELECT * FROM test'
+
+        return await this.query(queryString)
+    }
 
     static checkCancelledTests = async () => {
         let queryString = 'SELECT * FROM test WHERE finished =? AND start_date IS NOT NULL'
--- api/services/helpers.js
+++ api/services/helpers.js
@@ -5,20 +5,25 @@ exports.checkCancelledTests = async() => {
 
     setInterval(async function(){
 
-        let tests = await database.checkCancelledTests()
+        let exisitingTests = await database.checkTests()
 
-        for (let test of tests) {
+        if (exisitingTests.length > 0){
+            let tests = await database.checkCancelledTests()
 
-            let start_timestamp = parseInt((new Date(test.start_date).getTime() / 1000).toFixed(0));
-            let current_timestamp = parseInt((new Date() / 1000).toFixed(0));
+            for (let test of tests) {
+
+                let start_timestamp = parseInt((new Date(test.start_date).getTime() / 1000).toFixed(0));
+                let current_timestamp = parseInt((new Date() / 1000).toFixed(0));
 
 
 
-            if (current_timestamp - start_timestamp >360){ // 6 minutes
-                console.log("reset test " + test.url)
-                return await database.resetTest(test.url);
+                if (current_timestamp - start_timestamp >360){ // 6 minutes
+                    console.log("reset test " + test.url)
+                    return await database.resetTest(test.url);
+                }
             }
         }
+
 
     }, 60 * 1000);
 
